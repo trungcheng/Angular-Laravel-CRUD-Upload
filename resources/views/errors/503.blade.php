ae4a605d900be9b24d2a2fdcaa7154a4e30ac6b7
trungcheng <trungs1bmt@gmail.com>
2016-10-25 21:38:32 +0700

    see pull request

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -37,11 +37,11 @@
             }
         </style>
     </head>
-    <body>
+    <!-- <body>
         <div class="container">
             <div class="content">
                 <div class="title">Be right back.</div>
             </div>
         </div>
-    </body>
+    </body> -->
 </html>
